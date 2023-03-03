09d8e66b37a4cea4c05509a8ee7033917f0516dd
lakheraakshay <akshay.lakhera@easystepin.com>
2023-03-04 00:43:05 +0530

    done

--- versions/v1/helpers/init.mongodb.js
+++ versions/v1/helpers/init.mongodb.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-mongoose.connect(
-    'mongodb+srv://bobprep:bobprep@cluster0.izicijl.mongodb.net/?retryWrites=true&w=majority',
+mongoose
+  .connect(
+    "mongodb+srv://bobprep:bobprep@cluster0.izicijl.mongodb.net/?retryWrites=true&w=majority",
     {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
     }
-).then(() => { console.log('Database Connected') })
-    .catch(err => {
-        console.log(err.message,"<<<<< error")
-    });
+  )
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err.message, "<<<<< error");
+  });
