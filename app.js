e5cb7f00877d7e7e31b8adf4f7835fa5d2bdc8b5
lakheraakshay <akshay.lakhera@easystepin.com>
2023-03-07 17:21:12 +0530

    cors changes

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ require("dotenv").config();
 
 //Connection db
 require("./versions/v1/helpers/init.mongodb");
-
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(function (req, res, next) {
   console.log(req._parsedUrl.path, "----<<<<<<<<<<<Current ");
   res.setHeader("Access-Control-Allow-Origin", "*");
